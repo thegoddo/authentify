dd0184763bdd404cf85138a0504f3de2e9f1155e
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-10-01 01:44:59 +0530

    Initial Commit

---
+++ src/test/java/edu/project/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.project.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
