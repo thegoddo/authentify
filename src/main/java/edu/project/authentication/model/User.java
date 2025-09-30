8606afe9dfcea9e06b7ac4b426fe4c4d5bbf160e
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-10-01 02:02:52 +0530

    add data check

--- src/main/java/edu/project/authentication/model/User.java
+++ src/main/java/edu/project/authentication/model/User.java
@@ -18,14 +18,19 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String role = "ROLE_USER";
 
+    @Column(nullable = false)
     private boolean enabled = false;
 
+    @Column(nullable = false)
     private boolean accountNonLocked = true;
 
     private String resetToken;
