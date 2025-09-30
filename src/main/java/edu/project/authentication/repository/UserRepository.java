80e5c16bbc561c654ea4f798614b5066e01c4faf
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-10-01 02:08:27 +0530

    create JPA repository for User class

---
+++ src/main/java/edu/project/authentication/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package edu.project.authentication.repository;
+
+import edu.project.authentication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByResetToken(String resetToken);
+}
