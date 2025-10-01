885e9f8557939334298d3e0b15291097f9931ee8
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-10-01 12:03:00 +0530

    Implement Spring Security's UserDetailsService

--- src/main/java/edu/project/authentication/service/CustomUserDetailService.java
+++ src/main/java/edu/project/authentication/service/CustomUserDetailService.java
@@ -1,16 +1,35 @@
 package edu.project.authentication.service;
 
+import edu.project.authentication.model.User;
 import edu.project.authentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailService {
+public class CustomUserDetailService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Autowired
-    public UserDetailService(UserRepository userRepository) {
+    public CustomUserDetailService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(email)
+                .password(user.getPassword()) // Use hashed password
+                .roles(user.getRole().replace("ROLE_", "")) // Remove "ROLE_" prefix if present
+                .disabled(!user.isEnabled())
+                .accountLocked(!user.isAccountNonLocked())
+                .build();
     }
 }
